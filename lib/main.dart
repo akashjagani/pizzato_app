6fef8c93c7cee05aeee5ea5e6a575c6c6c7c295e
Aakash <akashjagani4899@gmail.com>
2021-02-28 10:40:42 +0530

    akash commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('akash bhaiiiii');
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(value: Headers()),
--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -1,14 +1,11 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:intl/intl.dart';
 
 class DetailsScreen extends StatefulWidget {
   @override
   _DetailsScreenState createState() => _DetailsScreenState();
 }
-
-String date = "2021-02-26T16:15:38.000Z";
 
 class _DetailsScreenState extends State<DetailsScreen> {
   @override
